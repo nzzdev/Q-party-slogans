a78d42e3721ef8a4101954bcb4a9ea4a7d8946e0
Beni <beni.buess@nzz.ch>
2016-08-17 14:09:41 +0200

    fixes bundled module name

--- build/tasks/bundle.js
+++ build/tasks/bundle.js
@@ -18,7 +18,7 @@ var builder = new Builder('','config.js');
 
 builder.config({
   paths: {
-    'party-slogans/*': 'dist/system/*',
+    'q-party-slogans/*': 'dist/system/*',
     'github:*': 'jspm_packages/github/*',
     'npm:*': 'jspm_packages/npm/*'
   },
@@ -31,7 +31,7 @@ gulp.task('copy-theme-for-deployment', function() {
 
 gulp.task('bundle-js', function(callback) {
   builder
-    .bundle('party-slogans/index', bundleName + '.js', { normalize: true, minify: true })
+    .bundle('q-party-slogans/index', bundleName + '.js', { normalize: true, minify: true })
     .then(function() {
       callback();
     })

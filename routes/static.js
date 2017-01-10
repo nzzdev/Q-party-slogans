0c30868f2486258ebd8e52edad51634c1c2e58c7
Beni <beni.buess@nzz.ch>
2017-01-10 14:52:24 +0100

    removes sophie style url from response

--- routes/static.js
+++ routes/static.js
@@ -23,11 +23,8 @@ module.exports = {
     let data = {
       stylesheets: [
         {
-          url: 'https://service.sophie.nzz.ch/bundle/sophie-q@~0.1.1,sophie-font@^0.1.0,sophie-color@~1.0.0,sophie-viz-color@^1.0.0[diverging-6].css',
+          name: 'default',
           type: 'critical'
-        },
-        {
-          rendererStyle: 'default'
         }
       ],
       markup: staticTpl.render(request.payload.item)

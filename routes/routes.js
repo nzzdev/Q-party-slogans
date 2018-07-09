903925d9c2057dc4a7bba472d9c657090d76d6e0
Philip Küng <kueng.philip@gmail.com>
2018-07-09 12:41:23 +0200

    add translations

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,8 @@
 module.exports = [
-  require('./schema.js'),
-  require('./stylesheet.js'),
-  require('./rendering-info/html-static.js'),
-  require('./health.js'),
-  require('./fixtures/data.js')
-]
+  require("./schema.js"),
+  require("./stylesheet.js"),
+  require("./rendering-info/html-static.js"),
+  require("./health.js"),
+  require("./locales.js"),
+  require("./fixtures/data.js")
+];
